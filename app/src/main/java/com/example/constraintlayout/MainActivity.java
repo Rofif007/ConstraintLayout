9f5acb1bb3c4051b8aa334440beeaded2744057b
Rofif007 <79833612+Rofif007@users.noreply.github.com>
2021-03-08 16:19:30 +0700

    20190140007_RofifAthoyaNurPrabu

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.constraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnoke;
+    EditText edEmail, edPassword;
+    String Email, Password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnoke = findViewById(R.id.btnoke);
+        edEmail = findViewById(R.id.edEmail);
+        edPassword = findViewById(R.id.edPassword);
+
+        btnoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Email = edEmail.getText().toString();
+                Password = edPassword.getText().toString();
+
+                Toast t = Toast.makeText(getApplicationContext(),
+                         " dan Email " + Email + " dan Password anda: " + Password + " ", Toast.LENGTH_LONG);
+                t.show();
+            }
+            public boolean validasiData(){
+                String emailActive = "rofif@gmail.com";
+                String passActive = "Wek";
+
+                if(emailActive.equals(edEmail.getText().toString()) && passActive.equals(edPassword.getText().toString())){
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
